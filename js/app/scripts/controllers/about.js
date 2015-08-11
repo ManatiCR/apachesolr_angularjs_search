c9d4bb6625af20d30714737c7e5620821eecebb6
Kevin Porras <kporras07@gmail.com>
2015-08-11 16:49:52 -0600

    Scaffolded Angular project.

---
+++ js/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jsApp
+ */
+angular.module('jsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ js/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jsApp
+ */
+angular.module('jsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
