77dad318812585c80db435d52170662ea72c9535
Kevin Porras <kporras07@gmail.com>
2015-08-13 14:21:26 -0600

    Added controller and ng-app into template.

--- templates/apachesolr-angularjs-advanced-search.tpl.php
+++ templates/apachesolr-angularjs-advanced-search.tpl.php
@@ -4,17 +4,14 @@
  * @file
  * AngularJS template to render
  */
-
 ?>
 
-<div class="advancedSearch" id="advancedSearch" data-ng-app="jsApp">
+<div class="advancedSearch" id="advancedSearch" data-ng-app="search">
   <!--[if lte IE 8]>
     <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
   <![endif]-->
 
-  <div class="container">
-    <div ng-view=""></div>
-  </div>
+  <div class="container" data-ng-controller="mainController">
 
 <pre>{{fields}}</pre>
   <form class="advanced-search-form" action="#">
@@ -24,4 +21,5 @@
     </div>
   </form>
 
+</div>
 </div>
