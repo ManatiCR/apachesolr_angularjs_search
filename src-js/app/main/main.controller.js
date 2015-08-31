fa87e31d49eef0cc6ee400618253af789ffee964
Kevin Porras <kporras07@gmail.com>
2015-08-31 15:37:47 -0600

    JS changes to support 2 previous commits.

--- src-js/app/main/main.controller.js
+++ src-js/app/main/main.controller.js
@@ -15,6 +15,7 @@
 
       main.clearForm = clearForm;
       main.processForm = processForm;
+      main.fieldChanged = fieldChanged;
       main.fields = {};
 
       // Unbind the event.
@@ -26,41 +27,71 @@
       var pageId = data.pageId;
 
       var field;
-      for (field in fields) {
-        if (fields[field].type === 'boolean') {
-          fields[field].type = 'checkbox';
-        }
-        else if (fields[field].type === 'string') {
-          fields[field].type = 'text';
-        }
-        else if (fields[field].type === 'numeric') {
-          fields[field].type = 'number';
+      var status;
+      for (status in fields) {
+        for (field in fields[status]) {
+          if (fields[status][field].type === 'boolean') {
+            fields[status][field].type = 'checkbox';
+          }
+          else if (fields[status][field].type === 'string') {
+            fields[status][field].type = 'text';
+          }
+          else if (fields[status][field].type === 'numeric') {
+            fields[status][field].type = 'number';
+          }
         }
       }
       main.fields = fields;
+      main.fields.active = main.fields.always;
+      main.selectedField = main.fields.active[Object.keys(main.fields.active)[0]].id;
+
+      function fieldChanged(index) {
+        var selectedField = main.selectedField;
+        var actualIndex = 0;
+        var field;
+        for (field in main.fields.active) {
+          if (actualIndex === index) {
+            var thisField = getField(selectedField);
+            if (field) {
+              main.fields.active[field] = thisField;
+              break;
+            }
+          }
+          actualIndex++;
+        }
+      }
+
+      function getField(fieldId) {
+        for (field in main.fields.selected) {
+          if (main.fields.selected[field].id === fieldId) {
+            return main.fields.selected[field];
+          }
+        }
+        return false;
+      }
 
       function clearForm() {
-        for (field in main.fields) {
-          main.fields[field].value = '';
+        for (field in main.fields.active) {
+          main.fields.active[field].value = '';
         }
       }
 
       function processForm() {
         var string = '';
         var filter = '';
-        for (field in main.fields) {
-          if (main.fields[field].value) {
-            if (main.fields[field].type === 'checkbox') {
-              filter += field;
+        for (field in main.fields.active) {
+          if (main.fields.active[field].value) {
+            if (main.fields.active[field].type === 'checkbox') {
+              filter += main.fields.active[field].id;
               filter += ':true';
               filter += ' OR ';
             }
             else {
               if (field !== '__fulltext_search') {
-                string += field;
+                string += main.fields.active[field].id;
                 string += ':';
               }
-              string += main.fields[field].value;
+              string += main.fields.active[field].value;
               string += ' AND ';
             }
           }
