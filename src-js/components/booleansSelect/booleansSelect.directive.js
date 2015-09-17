f27fe8041fde4ce063eb9a6400347f05bb49d617
Kevin Porras <kporras07@gmail.com>
2015-09-16 21:47:51 -0600

    Added new booleansSelect directive.

---
+++ src-js/components/booleansSelect/booleansSelect.directive.js
@@ -0,0 +1,40 @@
+angular
+  .module('apachesolrAngularjsSearch')
+  .directive('aasBooleansSelect', booleansSelect);
+
+function booleansSelect($rootScope, drupalDataService) {
+
+  var basePath;
+  $rootScope.$on('drupalDataReady', function() {
+    var data = drupalDataService.getDrupalData();
+    basePath = data.modulePath;
+  });
+
+  var directive = {
+    // @TODO: Change hardcoded path.
+    templateUrl: 'sites/all/modules/custom/apachesolr_angularjs_search' + '/src-js/components/booleansSelect/booleans-select.html',
+    restrict: 'A',
+    scope: {
+      options: '=aasBooleansSelectOptions',
+      selected: '=ngModel'
+    },
+    controller: BooleansSelectController,
+    controllerAs: 'vm',
+    bindToController: true
+  };
+  return directive;
+
+  function BooleansSelectController($scope) {
+    var vm = this;
+    vm.expanded = false;
+    console.log(vm);
+
+    vm.selectItem = selectItem;
+
+    function selectItem(item) {
+      vm.selected = item;
+      vm.expanded = false;
+    }
+
+  }
+}
