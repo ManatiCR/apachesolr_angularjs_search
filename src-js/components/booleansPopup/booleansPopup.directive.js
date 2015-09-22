a16294bb964e39bcf18104364fa43f979adc87c4
Kevin Porras <kporras07@gmail.com>
2015-09-22 16:17:43 -0600

    Added missing IIFE in directives.

--- src-js/components/booleansPopup/booleansPopup.directive.js
+++ src-js/components/booleansPopup/booleansPopup.directive.js
@@ -1,151 +1,157 @@
-angular
-  .module('apachesolrAngularjsSearch')
-  .directive('aasBooleansPopup', booleansPopup);
+/**
+ * apachesolrAngularjsSearch aasBooleansPopup directive.
+ *
+ */
 
-function booleansPopup($rootScope, drupalDataService) {
+(function () {
+  'use strict';
+  angular.module('apachesolrAngularjsSearch').directive('aasBooleansPopup', booleansPopup);
 
-  var basePath;
-  $rootScope.$on('drupalDataReady', function() {
-    var data = drupalDataService.getDrupalData();
-    basePath = data.modulePath;
-  });
+  function booleansPopup($rootScope, drupalDataService) {
 
-  var directive = {
-    // @TODO: Change hardcoded path.
-    templateUrl: '/sites/all/modules/custom/apachesolr_angularjs_search' + '/src-js/components/booleansPopup/booleans-popup.html',
-    restrict: 'E',
-    scope: {
-      field: '='
-    },
-    controller: BooleansPopupController,
-    controllerAs: 'vm',
-    bindToController: true
-  };
-  return directive;
+    var basePath;
+    $rootScope.$on('drupalDataReady', function() {
+      var data = drupalDataService.getDrupalData();
+      basePath = data.modulePath;
+    });
 
-  function BooleansPopupController($scope) {
-    var vm = this;
-
-    vm.parentScope = $scope.$parent;
-    vm.booleansPopup = vm.parentScope.main.booleansPopup = {show: false};
-    vm.textChange = textChange;
-    vm.addBoolean = addBoolean;
-    vm.highlightChange = highlightChange;
-    vm.optionSelected = optionSelected;
-    vm.format = {
-      '#619FFF': /\sAND|\sOR|\sNOT/g
+    var directive = {
+      // @TODO: Change hardcoded path.
+      templateUrl: '/sites/all/modules/custom/apachesolr_angularjs_search' + '/src-js/components/booleansPopup/booleans-popup.html',
+      restrict: 'E',
+      scope: {
+        field: '='
+      },
+      controller: BooleansPopupController,
+      controllerAs: 'vm',
+      bindToController: true
     };
-    vm.firstBoolean = '';
+    return directive;
 
-    var target;
-    var selectionStart;
+    function BooleansPopupController($scope) {
+      var vm = this;
 
-    if (!vm.field.value) {
-      // Prevent error with hightlight.
-      vm.field.value = '';
-    }
+      vm.parentScope = $scope.$parent;
+      vm.booleansPopup = vm.parentScope.main.booleansPopup = {show: false};
+      vm.textChange = textChange;
+      vm.addBoolean = addBoolean;
+      vm.highlightChange = highlightChange;
+      vm.optionSelected = optionSelected;
+      vm.format = {
+        '#619FFF': /\sAND|\sOR|\sNOT/g
+      };
+      vm.firstBoolean = '';
 
-    function textChange($event) {
-      if (!target) {
-        target = $event.target;
+      var target;
+      var selectionStart;
+
+      if (!vm.field.value) {
+        // Prevent error with hightlight.
+        vm.field.value = '';
       }
 
-      if ($event.charCode === 44) {
-        vm.booleansPopup.show = true;
-        selectionStart = target.selectionStart;
-        target.focus();
-      }
-      else {
-        vm.booleansPopup.show = false;
-      }
-      return true;
-    }
-
-    function addBoolean(operator) {
-
-      var addBooleanInField = true;
-      vm.booleansPopup.show = false;
-      if (vm.field.id !== '__fulltext_search' && vm.firstBoolean && vm.firstBoolean !== operator) {
-        addBooleanInField = false;
-      }
-
-      if (addBooleanInField) {
-
-        if (!vm.firstBoolean) {
-          vm.firstBoolean = operator;
+      function textChange($event) {
+        if (!target) {
+          target = $event.target;
         }
 
-        if (vm.type === 'autocomplete') {
-          vm.field.value.push({id: operator, name: operator, class:'advanced-search--field-autocomplete-operator'});
+        if ($event.charCode === 44) {
+          vm.booleansPopup.show = true;
+          selectionStart = target.selectionStart;
+          target.focus();
         }
         else {
-          var part = vm.field.value.substr(selectionStart + 1);
-          vm.field.value = vm.field.value.substr(0, selectionStart);
-          vm.field.value += ' ' + operator + ' ';
-          vm.field.value += part;
+          vm.booleansPopup.show = false;
         }
-        var selectionSum = operator.length + 2;
-        var cursorPosition = selectionStart + selectionSum;
-        if (target) {
-          target.focus();
-          setTimeout(function() {
-            // Let's wait focus has finished before applying selectionRange.
-            target.setSelectionRange(cursorPosition, cursorPosition);
-          }, 0);
-        }
+        return true;
       }
-      else {
-        if (vm.type !== 'autocomplete') {
-          var part = vm.field.value.substr(selectionStart + 1);
-          vm.field.value = vm.field.value.substr(0, selectionStart);
-          vm.field.value += part;
-        }
-        vm.field.nextConnector = operator.toLowerCase();
-        $scope.$parent.main.addSameField($scope.$parent.groupIndex, $scope.$parent.$index);
-        if (target) {
-          setTimeout(function() {
-        //    angular.element(target).parents('.advanced-search--field-container').next().find('.advanced-search--field-value').focus();
-          }, 0);
-        }
-      }
-    }
 
-    function highlightChange($markers) {
-      var lastInserted = {};
-      // @TODO: Fix random behaviors when boolean is not inserted at end.
-      if ($markers.length) {
-        lastInserted = $markers[$markers.length - 1];
-        vm.firstBoolean = $markers[0].text.trim();
+      function addBoolean(operator) {
 
-        if (vm.field.id !== '__fulltext_search' && vm.firstBoolean !== lastInserted.text.trim()) {
-          vm.field.value = vm.field.value.substr(0, vm.field.value.length - 3);
-          vm.field.nextConnector = lastInserted.text.toLowerCase().trim();
+        var addBooleanInField = true;
+        vm.booleansPopup.show = false;
+        if (vm.field.id !== '__fulltext_search' && vm.firstBoolean && vm.firstBoolean !== operator) {
+          addBooleanInField = false;
+        }
+
+        if (addBooleanInField) {
+
+          if (!vm.firstBoolean) {
+            vm.firstBoolean = operator;
+          }
+
+          if (vm.type === 'autocomplete') {
+            vm.field.value.push({id: operator, name: operator, class:'advanced-search--field-autocomplete-operator'});
+          }
+          else {
+            var part = vm.field.value.substr(selectionStart + 1);
+            vm.field.value = vm.field.value.substr(0, selectionStart);
+            vm.field.value += ' ' + operator + ' ';
+            vm.field.value += part;
+          }
+          var selectionSum = operator.length + 2;
+          var cursorPosition = selectionStart + selectionSum;
+          if (target) {
+            target.focus();
+            setTimeout(function() {
+              // Let's wait focus has finished before applying selectionRange.
+              target.setSelectionRange(cursorPosition, cursorPosition);
+            }, 0);
+          }
+        }
+        else {
+          if (vm.type !== 'autocomplete') {
+            var part = vm.field.value.substr(selectionStart + 1);
+            vm.field.value = vm.field.value.substr(0, selectionStart);
+            vm.field.value += part;
+          }
+          vm.field.nextConnector = operator.toLowerCase();
           $scope.$parent.main.addSameField($scope.$parent.groupIndex, $scope.$parent.$index);
-          setTimeout(function() {
-            angular.element(target).parents('.advanced-search--field-container').next().find('.advanced-search--field-value').focus();
-          }, 0);
+          if (target) {
+            setTimeout(function() {
+              //    angular.element(target).parents('.advanced-search--field-container').next().find('.advanced-search--field-value').focus();
+            }, 0);
+          }
         }
       }
-      else {
-        vm.firstBoolean = '';
-      }
-    }
 
-    function optionSelected() {
-      if (vm.field.value) {
-        vm.booleansPopup.show = true;
-        vm.type = 'autocomplete';
-        angular.element(document.getElementsByClassName('ui-select-search')).on('keydown', hideBooleansPopup);
-      }
-      else {
-        vm.booleansPopup.show = true;
-      }
-    }
+      function highlightChange($markers) {
+        var lastInserted = {};
+        // @TODO: Fix random behaviors when boolean is not inserted at end.
+        if ($markers.length) {
+          lastInserted = $markers[$markers.length - 1];
+          vm.firstBoolean = $markers[0].text.trim();
 
-    function hideBooleansPopup() {
-      vm.booleansPopup.show = false;
-      angular.element(document.getElementsByClassName('ui-select-search')).unbind('keydown', hideBooleansPopup);
-    }
+          if (vm.field.id !== '__fulltext_search' && vm.firstBoolean !== lastInserted.text.trim()) {
+            vm.field.value = vm.field.value.substr(0, vm.field.value.length - 3);
+            vm.field.nextConnector = lastInserted.text.toLowerCase().trim();
+            $scope.$parent.main.addSameField($scope.$parent.groupIndex, $scope.$parent.$index);
+            setTimeout(function() {
+              angular.element(target).parents('.advanced-search--field-container').next().find('.advanced-search--field-value').focus();
+            }, 0);
+          }
+        }
+        else {
+          vm.firstBoolean = '';
+        }
+      }
 
+      function optionSelected() {
+        if (vm.field.value) {
+          vm.booleansPopup.show = true;
+          vm.type = 'autocomplete';
+          angular.element(document.getElementsByClassName('ui-select-search')).on('keydown', hideBooleansPopup);
+        }
+        else {
+          vm.booleansPopup.show = true;
+        }
+      }
+
+      function hideBooleansPopup() {
+        vm.booleansPopup.show = false;
+        angular.element(document.getElementsByClassName('ui-select-search')).unbind('keydown', hideBooleansPopup);
+      }
+
+    }
   }
-}
+})();
--- src-js/components/booleansSelect/booleansSelect.directive.js
+++ src-js/components/booleansSelect/booleansSelect.directive.js
@@ -1,39 +1,45 @@
-angular
-  .module('apachesolrAngularjsSearch')
-  .directive('aasBooleansSelect', booleansSelect);
+/**
+ * apachesolrAngularjsSearch aasBooleansSelect directive.
+ *
+ */
 
-function booleansSelect($rootScope, drupalDataService) {
+(function () {
+  'use strict';
+  angular.module('apachesolrAngularjsSearch').directive('aasBooleansSelect', booleansSelect);
 
-  var basePath;
-  $rootScope.$on('drupalDataReady', function() {
-    var data = drupalDataService.getDrupalData();
-    basePath = data.modulePath;
-  });
+  function booleansSelect($rootScope, drupalDataService) {
 
-  var directive = {
-    // @TODO: Change hardcoded path.
-    templateUrl: '/sites/all/modules/custom/apachesolr_angularjs_search' + '/src-js/components/booleansSelect/booleans-select.html',
-    restrict: 'A',
-    scope: {
-      options: '=aasBooleansSelectOptions',
-      selected: '=ngModel'
-    },
-    controller: BooleansSelectController,
-    controllerAs: 'vm',
-    bindToController: true
-  };
-  return directive;
+    var basePath;
+    $rootScope.$on('drupalDataReady', function() {
+      var data = drupalDataService.getDrupalData();
+      basePath = data.modulePath;
+    });
 
-  function BooleansSelectController($scope) {
-    var vm = this;
-    vm.expanded = false;
+    var directive = {
+      // @TODO: Change hardcoded path.
+      templateUrl: '/sites/all/modules/custom/apachesolr_angularjs_search' + '/src-js/components/booleansSelect/booleans-select.html',
+      restrict: 'A',
+      scope: {
+        options: '=aasBooleansSelectOptions',
+        selected: '=ngModel'
+      },
+      controller: BooleansSelectController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+    return directive;
 
-    vm.selectItem = selectItem;
-
-    function selectItem(item) {
-      vm.selected = item;
+    function BooleansSelectController($scope) {
+      var vm = this;
       vm.expanded = false;
-    }
 
+      vm.selectItem = selectItem;
+
+      function selectItem(item) {
+        vm.selected = item;
+        vm.expanded = false;
+      }
+
+    }
   }
-}
+})();
